460f18e550e3bc8666088261eeb2763d5827c2cb
SEUNG WON JEONG <SEUNG WON JEONG@DESKTOP-MS92HU8>
2023-12-20 18:59:16 +0900

    Change content.

--- MyGit/src/MyGit/A.java
+++ MyGit/src/MyGit/A.java
@@ -4,5 +4,6 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("A class is maded.");
 		System.out.println("A class is modified");
+		System.out.println("Computer Science");
 	}
 }
